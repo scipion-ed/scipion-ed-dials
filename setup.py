a8e3141dc42f97732e4a29b9075de7033911239c
ViktorEGBengtsson <viktor.bengtsson@mmk.su.se>
2021-04-22 09:50:26 +0200

    Fix typo in classifier

--- setup.py
+++ setup.py
@@ -112,7 +112,7 @@ setup(
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 5 - Production',
+        'Development Status :: 5 - Production/Stable',
         # Indicate who your project is intended for
         'Intended Audience :: Science/Research',
         # Pick your license as you wish
