4509388a53ead3dfbbd93c91ddf9e87a5311a4ab
ViktorEGBengtsson <viktor.e.g.bengtsson@gmail.com>
2022-02-07 17:03:58 +0100

    Add DIALS citation to base protocol

---
+++ dials/bibtex.py
@@ -0,0 +1,46 @@
+# coding: latin-1
+# **************************************************************************
+# *
+# * Authors:     J.M. De la Rosa Trevin (delarosatrevin@scilifelab.se) [1]
+# *              V. E.G: Bengtsson (viktor.bengtsson@mmk.su.se) [2]
+# *
+# * [1] SciLifeLab, Stockholm University
+# * [2] Department of Materials and Environmental Chemistry, Stockholm University
+# *
+# * This program is free software; you can redistribute it and/or modify
+# * it under the terms of the GNU General Public License as published by
+# * the Free Software Foundation; either version 3 of the License, or
+# * (at your option) any later version.
+# *
+# * This program is distributed in the hope that it will be useful,
+# * but WITHOUT ANY WARRANTY; without even the implied warranty of
+# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# * GNU General Public License for more details.
+# *
+# * You should have received a copy of the GNU General Public License
+# * along with this program; if not, write to the Free Software
+# * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+# * 02111-1307  USA
+# *
+# *  All comments concerning this program package may be sent to the
+# *  e-mail address 'scipion@cnb.csic.es'
+# *
+# **************************************************************************
+
+"""
+
+@article{Winter2018,
+title = "DIALS: implementation and evaluation of a new integration package",
+journal = "Acta Crystallographica Section D",
+volume = "74",
+number = "2",
+pages = "85--97",
+year = "2018",
+doi = "http://dx.doi.org/10.1107/S2059798317017235",
+url = "https://journals.iucr.org/d/issues/2018/02/00/di5011/index.html",
+author = "Winter, Graeme and Waterman, David G. and Parkhurst, James M. and Brewster, Aaron S. and Gildea, Richard J. and Gerstel, Markus and Fuentes-Montero, Luis and Vollmar, Melanie and Michels-Clark, Tara and Young, Iris D. and Sauter, Nicholas K. and Evans, Gwyndaf",
+}
+
+
+
+"""
