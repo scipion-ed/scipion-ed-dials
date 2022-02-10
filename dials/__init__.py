973d467b898366f954306d3c1405da9595743b6a
ViktorEGBengtsson <viktor.bengtsson@mmk.su.se>
2022-02-10 15:43:09 +0100

    Iterate version number to 3!1.0.0b3

--- dials/__init__.py
+++ dials/__init__.py
@@ -35,7 +35,7 @@ from .constants import *
 
 # Epoch indicates compatible main Scipion version
 # major.minor.micro versioning starting with 1.0.0 in the new epoch
-__version__ = '3!1.0.0b2'
+__version__ = '3!1.0.0b3'
 _logo = "logo.jpeg"
 
 
