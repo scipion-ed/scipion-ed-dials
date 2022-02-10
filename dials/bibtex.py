54116604f52663341e215b6065d7fb4469cf2013
ViktorEGBengtsson <viktor.bengtsson@mmk.su.se>
2022-02-10 11:33:51 +0100

    Add citation information about underlying software to methods tab Fixes #38

--- dials/bibtex.py
+++ dials/bibtex.py
@@ -29,18 +29,35 @@
 
 """
 
-@article{Winter2018,
-title = "DIALS: implementation and evaluation of a new integration package",
+@article{Winter:di5011,
+author = "Winter, Graeme and Waterman, David G. and Parkhurst, James M. and Brewster, Aaron S. and Gildea, Richard J. and Gerstel, Markus and Fuentes-Montero, Luis and Vollmar, Melanie and Michels-Clark, Tara and Young, Iris D. and Sauter, Nicholas K. and Evans, Gwyndaf",
+title = "{{\it DIALS}: implementation and evaluation of a new integration package}",
 journal = "Acta Crystallographica Section D",
+year = "2018",
 volume = "74",
 number = "2",
 pages = "85--97",
-year = "2018",
-doi = "http://dx.doi.org/10.1107/S2059798317017235",
-url = "https://journals.iucr.org/d/issues/2018/02/00/di5011/index.html",
-author = "Winter, Graeme and Waterman, David G. and Parkhurst, James M. and Brewster, Aaron S. and Gildea, Richard J. and Gerstel, Markus and Fuentes-Montero, Luis and Vollmar, Melanie and Michels-Clark, Tara and Young, Iris D. and Sauter, Nicholas K. and Evans, Gwyndaf",
+month = "Feb",
+doi = {http://doi.org/10.1107/S2059798317017235},
+url = {http://doi.org/10.1107/S2059798317017235},
+abstract = {The {\it DIALS} project is a collaboration between Diamond Light Source, Lawrence Berkeley National Laboratory and CCP4 to develop a new software suite for the analysis of crystallographic X-ray diffraction data, initially encompassing spot finding, indexing, refinement and integration. The design, core algorithms and structure of the software are introduced, alongside results from the analysis of data from biological and chemical crystallography experiments.},
+keywords = {X-ray diffraction, data processing, methods development, DIALS},
 }
 
+@article{Gildea:rr5155,
+author = "Gildea, Richard J. and Winter, Graeme",
+title = "{Determination of Patterson group symmetry from sparse multi-crystal data sets in the presence of an indexing ambiguity}",
+journal = "Acta Crystallographica Section D",
+year = "2018",
+volume = "74",
+number = "5",
+pages = "405--410",
+month = "May",
+doi = {http://doi.org/10.1107/S2059798318002978},
+url = {http://doi.org/10.1107/S2059798318002978},
+abstract = {Combining X-ray diffraction data from multiple samples requires determination of the symmetry and resolution of any indexing ambiguity. For the partial data sets typical of {\it in situ} room-temperature experiments, determination of the correct symmetry is often not straightforward. The potential for indexing ambiguity in polar space groups is also an issue, although methods to resolve this are available if the true symmetry is known. Here, a method is presented to simultaneously resolve the determination of the Patterson symmetry and the indexing ambiguity for partial data sets.},
+keywords = {Patterson group symmetry, partial data sets, indexing ambiguity},
+}
 
 
 """
